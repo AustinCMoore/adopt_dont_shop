b3a68c9347ef61f67a4842149665050a8458c1c3
Austin Moore <austinchristianmoore@gmail.com>
2022-02-15 18:25:42 -0500

    Feature: Create render pending apps test

--- spec/features/admins/index_spec.rb
+++ spec/features/admins/index_spec.rb
@@ -5,9 +5,18 @@ RSpec.describe 'the shelters index' do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-    @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    @pet_3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @pet_4 = @shelter_2.pets.create(name: 'Blinky', breed: 'Bengal', age: 2, adoptable: true)
+    @application_1 = Application.create!(name: "Austin Moore", street_address: "9999 Imaginary Ave", city: "Laurel", state: "MD", zip_code: "99999", description: "I am very nice", full_address: "9999 Imaginary Ave, Laurel, MD 99999", status: :pending)
+    @application_2 = Application.create!(name: "Noel Sitnick", street_address: "9999 Make Believe Rd", city: "Scaggsville", state: "MD", zip_code: "00001", description: "I am very sweet", full_address: "9999 Make Believe Rd, Scaggsville, MD 00001", status: :pending)
+    @application_3 = Application.create!(name: "Daniel Sitnick", street_address: "9999 Made up street", city: "Baltimore", state: "MD", zip_code: "10001", description: "TBD", full_address: "9999 Make Believe Rd, Scaggsville, MD 00001", status: :in_progress)
+    @pet_application_1 = PetApplication.create!(pet: @pet_1, application: @application_1, status: :pending)
+    @pet_application_2 = PetApplication.create!(pet: @pet_4, application: @application_1, status: :pending)
+    @pet_application_3 = PetApplication.create!(pet: @pet_2, application: @application_2, status: :pending)
+    @pet_application_4 = PetApplication.create!(pet: @pet_3, application: @application_3, status: :pending)
+
     visit "/admin/shelters"
   end
 
@@ -36,6 +45,14 @@ RSpec.describe 'the shelters index' do
 
     within "#shelter-#{@shelter_1.id}" do
       expect(page).to have_content("Name: #{@shelter_1.name}")
+    end
+  end
+
+  it "displays pending applications" do
+    within '#pending-applications' do
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_2.name)
+      expect(page).to_not have_content(@shelter_3.name)
     end
   end
 end
