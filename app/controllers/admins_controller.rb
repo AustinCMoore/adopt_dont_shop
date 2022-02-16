7e6e8822dc3bb0f66e5d48d37e2027887d359ebb
Austin Moore <austinchristianmoore@gmail.com>
2022-02-15 21:34:14 -0500

    Feature: Create render functionality

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,6 @@
 class AdminsController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name desc")
-    @pending_shelters
+    @pending_shelters = Shelter.select_shelters_w_pending_apps
   end
 end
